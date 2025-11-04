10ec0fb72ab1f740afaa40e9a89ee6dd19d46487
JH_Lab1 <jiaheng.24@ichat.sp.edu.sg>
2025-11-04 14:27:03 +0800

    added return values

--- bmi.py
+++ bmi.py
@@ -5,9 +5,12 @@ def calculate_bmi(height,weight):
     print(bmi)
     if(bmi>25.0):
         print("You are Over Weight")
+        return 1
     elif(bmi>=18.5):
         print("You are Normal Weight")
+        return 0
     else:
         print("You are Under Weight")
+        return -1
 
 calculate_bmi(weight=57,height=1.73)
